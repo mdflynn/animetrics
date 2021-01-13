6008f66fae4c83efca8e6c0880689ffd111c4dec
mdflynn <mdflynn34@outlook.com>
2021-01-13 08:42:27 -0700

    Remove unnecessary files and code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
+const router = (
+  <MemoryRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MemoryRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(router, document.getElementById("root"));
