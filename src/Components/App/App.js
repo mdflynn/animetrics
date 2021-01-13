5e61f16fb03e79595e3d84b6fb85ea76ec5f1bb7
mdflynn <mdflynn34@outlook.com>
2021-01-13 11:28:07 -0700

    Add image to background

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MainPage from '../MainPage/MainPage';
 import './App.scss';
 
 const App = () => {
   return (
-    <h1>ANIMEtrics</h1>
+    <MainPage />
   )
 }
 
---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import allMight from '../../assets/allmight.jpeg'
+import './MainPage.scss';
+
+const MainPage = () => {
+    return (
+        <div className="div-bg">
+            <p>TEXT</p>
+        </div>
+    )
+}
+
+export default MainPage;
