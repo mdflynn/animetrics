8d6581081838f0d69d87955c7dbc58b027301e94
mdflynn <mdflynn34@outlook.com>
2021-01-14 16:35:33 -0700

    Add format date method

--- src/Components/EpisodeThumb/EpisodeThumb.js
+++ src/Components/EpisodeThumb/EpisodeThumb.js
@@ -2,12 +2,24 @@ import React from "react";
 import "./EpisodeThumb.scss";
 
 const EpisodeThumb = ({ data }) => {
+
+  const formatDate = (dateInfo) => {
+    const isolateDate = dateInfo.substr(0, 10);
+    const splitDate = isolateDate.split("-");
+    const date = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`;
+    return date;
+  };
+
+  
+
   return (
-    <div>
+    <article className="episode-thumb">
       <h1>{data.title}</h1>
-      <p>{data.aired}</p>
-      <a className="external-link" href={data.video_url}>Episode Details</a>
-    </div>
+      <p>{formatDate(data.aired)}</p>
+      <a className="external-link" href={data.video_url} target="_blank">
+        Episode Details
+      </a>
+    </article>
   );
 };
 
