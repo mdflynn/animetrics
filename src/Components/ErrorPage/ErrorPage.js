13a3036aad0c7a267b566d0461b8bc8b8bd2ce92
mdflynn <mdflynn34@outlook.com>
2021-01-15 18:31:32 -0700

    Create ErrorPage

---
+++ src/Components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ErrorPage.scss";
+
+const ErrorPage = () => {
+    return (
+        <div className="error">
+            <p>Page Not Found</p>
+        </div>
+    )
+}
+
+export default ErrorPage;
