e7319e95f2e87666f1089ef46183d2a050d87a52
mdflynn <mdflynn34@outlook.com>
2021-01-16 14:19:10 -0700

    Add aria labels

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -7,32 +7,32 @@ const MainPage = () => {
   return (
     <div className="div-bg">
       <div className="season-div">
-        <Link className="link" to="/season1">
+        <Link className="link" to="/season1" aria-label="Season Navigation">
           <article className="season-card" data-value="season-1">
             Season 1
           </article>
         </Link>
-        <Link className="link" to="/season2">
+        <Link className="link" to="/season2" aria-label="Season Navigation">
           <article className="season-card" data-value="season-2">
             Season 2
           </article>
         </Link>
-        <Link className="link" to="/season3">
+        <Link className="link" to="/season3" aria-label="Season Navigation">
           <article className="season-card" data-value="season-3">
             Season 3
           </article>
         </Link>
-        <Link className="link" to="/season4">
+        <Link className="link" to="/season4" aria-label="Season Navigation">
           <article className="season-card" data-value="season-4">
             Season 4
           </article>
         </Link>
-        <Link className="link" to="/season5">
+        <Link className="link" to="/season5" aria-label="Season Navigation">
           <article className="season-card" data-value="season-5">
             Season 5
           </article>
         </Link>
-        <Link className="link" to="/movies">
+        <Link className="link" to="/movies" aria-label="Movies Navigation">
           <article className="season-card" data-value="movies">
             Movies
           </article>
